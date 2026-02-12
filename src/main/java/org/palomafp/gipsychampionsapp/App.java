22b213bad724ac9fe72a92e807c871859b06c8de
Adrián Sandoval <Bichito2509@gmail.com>
2026-02-12 09:19:38 +0100

    menu semicreado

--- src/main/java/org/palomafp/gipsychampionsapp/App.java
+++ src/main/java/org/palomafp/gipsychampionsapp/App.java
@@ -1,45 +1,44 @@
 package org.palomafp.gipsychampionsapp;
 
-import java.util.List;
-
-import org.palomafp.gipsychampionsapp.modelo.Equipo;
-import org.palomafp.gipsychampionsapp.modelo.Jugador;
-import org.palomafp.gipsychampionsapp.modelo.StatsTempJugador;
+import java.util.Scanner;
 
 public class App {
 
 	public static void main(String[] args) {
-		Equipo equipo = EquipoDAO.getDemo();
-		List<Jugador> jugadores = equipo.getJugadores();
 
-		Jugador j1 = null;
-		Jugador j2 = null;
-		
-		if (jugadores != null && !jugadores.isEmpty()) {
-			j1 = jugadores.get(0);
-			if (jugadores.size() > 1) {
-				j2 = jugadores.get(1);
+		Scanner sc = new Scanner(System.in);
+		int respuesta, respuesta2;
+
+		do {
+			System.out.println("LIGA MORROCOTUDA");
+			System.out.println("----------------");
+			System.out.println("1. Equipo Random");
+			System.out.println("2. Equipo por Id");
+			System.out.println("3. Todos los equipos");
+			System.out.println("4. Salir");
+			respuesta = sc.nextInt();
+
+			switch (respuesta) {
+				case 1:
+
+					break;
+				case 2:
+					for (int i = 0; i < 10; i++){
+						System.out.println((i + 1) + ". ");
+					}
+					break;
+				case 3:
+
+					break;
+				case 4:
+					System.out.println("Saliendo...");
+					break;
+
+				default:
+					System.out.println("Ese número no es válido compañerete");
+					break;
 			}
-		}
-
-		StatsTempJugador s1 = new StatsTempJugador(5, 2, 900, 1, 0, 0, j1);
-		StatsTempJugador s2 = new StatsTempJugador(3, 4, 820, 2, 0, 0, j2);
-
-		System.out.println("--- Ejemplo de objetos del modelo ---");
-		System.out.println(equipo);
-		System.out.println(equipo.getJugadores());
-		System.out.println(equipo.getEstadio());
-
-		System.out.println("Jugadores:");
-		if (jugadores != null) {
-			for (Jugador j : jugadores) {
-				System.out.println("  - " + j);
-			}
-		}
-
-		System.out.println("\nStats temporales:");
-		System.out.println("  " + s1);
-		System.out.println("  " + s2);
+		} while (respuesta != 3);
 	}
 
 }
