9f1bc5a21cc2326fd8ff8daf20cdb52d7957f0a1
Adrián Sandoval <Bichito2509@gmail.com>
2026-02-25 14:04:10 +0100

    addequipo me quiero pegar un tiro

--- src/test/java/org/palomafp/gipsychampionsapp/EquipoDAOTest.java
+++ src/test/java/org/palomafp/gipsychampionsapp/EquipoDAOTest.java
@@ -2,15 +2,32 @@ package org.palomafp.gipsychampionsapp;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 import org.palomafp.gipsychampionsapp.modelo.Equipo;
 
 public class EquipoDAOTest {
-    
+
     @Test
     public void testGetEquipoAleatorio() {
         EquipoDAO equipoDAO = new EquipoDAO();
         Equipo equipoAleatorio = equipoDAO.getEquipoAleatorio();
         assertTrue(equipoDAO.getArrayList().contains(equipoAleatorio));
     }
+
+    @Test
+    public void addEquipo(){
+        EquipoDAO equipoDAO = new EquipoDAO();
+        ArrayList<Equipo> listaEquipo = new ArrayList<>();
+        boolean estaDisponible = true;
+
+        for (int i = 0; i < listaEquipo.size(); i++) {
+            if (listaEquipo.get(i).getIdEquipo() == idEquipo.) {
+                estaDisponible = false;
+            }
+    }
+
+    
 }
+
