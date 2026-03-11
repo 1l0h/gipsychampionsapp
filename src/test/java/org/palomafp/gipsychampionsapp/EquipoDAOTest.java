02aef34f415895b1e8de9aa6623e68b3134997a6
1l0h <ilobatomartin22@gmail.com>
2026-03-11 14:21:17 +0100

    equipoDAOTest comentado

--- src/test/java/org/palomafp/gipsychampionsapp/EquipoDAOTest.java
+++ src/test/java/org/palomafp/gipsychampionsapp/EquipoDAOTest.java
@@ -10,8 +10,18 @@ import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
 import org.palomafp.gipsychampionsapp.modelo.Equipo;
 
+/**
+ * Clase que hará las pruebas de la clase EquipoDAO
+ */
 public class EquipoDAOTest {
 
+    /**
+     * Caso de prueba que se encargará de comprobar que el método getEquipoAleatorio
+     * funciona correctamente, realizando comprobaciones con el array que forma
+     * parte de la instancia EquipoDAO,
+     * comprueba que el equipo forme parte del array, que el id del equipo no sea
+     * null y que el entrenador del equipo tampoco sea null.
+     */
     @Test
     public void testGetEquipoAleatorio() {
         EquipoDAO equipoDAO = new EquipoDAO();
@@ -23,6 +33,11 @@ public class EquipoDAOTest {
         assertTrue(equipoAleatorio.getEntrenador() != null, "El equipo debe tener un entrenador");
     }
 
+    /**
+     * Caso de prueba que prueba el indice 0 de la instancia de EquipoDAO,
+     * comprueba que no sea nulo, que el nombre concuerde con el establecido en la
+     * clase y que el entrenador sea el mismo que el establecido en la clase.
+     */
     @Test
     public void testGetEquipoPorIndiceCero() {
         EquipoDAO equipoDao = new EquipoDAO();
@@ -33,6 +48,11 @@ public class EquipoDAOTest {
         assertNotNull(equipoCero.getEntrenador(), "El entrenador no es correcto");
     }
 
+    /**
+     * Caso de prueba que prueba el indice final de la instancia de EquipoDAO,
+     * comprueba que no sea nulo, que el nombre concuerde con el establecido en la
+     * clase y que el entrenador sea el mismo que el establecido en la clase.
+     */
     @Test
     public void testGetEquipoPorIndiceFinal() {
         EquipoDAO equipoDao = new EquipoDAO();
@@ -42,6 +62,13 @@ public class EquipoDAOTest {
         assertNotNull(equipoFinal.getEntrenador(), "El entrenador no es correcto");
     }
 
+    /**
+     * Caso de prueba que prueba el arraylist de la instancia de EquipoDAO,
+     * prueba que el tamaño del array después de hacer la instancia de la clase sea
+     * de 4, que el nombre del entrenador del equipo en la posición 0 concuerde con
+     * el que debería, y que el nombre del jugador del equipo con posición 0 en el
+     * array concuerde con el que debería.
+     */
     @Test
     public void testGetArrayList() {
         EquipoDAO equipoDao = new EquipoDAO();
@@ -53,6 +80,11 @@ public class EquipoDAOTest {
                 "El jugador con id 6 del equipo con id 3 no es Sandofoca, por lo que es incorrecto.");
     }
 
+    /**
+     * Caso de prueba que prueba la adición de equipos
+     * prueba a añadir un equipo cuando el id de este ya está siendo usado y prueba
+     * añadir un equipo cuando el id de este está libre.
+     */
     @Test
     public void testAddEquipoEquipo() {
         EquipoDAO equipoDao = new EquipoDAO();
