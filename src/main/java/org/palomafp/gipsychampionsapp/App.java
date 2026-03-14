39d4c4ba29dfa0627a5d5a43551c3a4eb7c89063
1l0h <ilobatomartin22@gmail.com>
2026-03-14 13:41:01 +0100

    comentarios acabados

--- src/main/java/org/palomafp/gipsychampionsapp/App.java
+++ src/main/java/org/palomafp/gipsychampionsapp/App.java
@@ -8,6 +8,7 @@ import org.palomafp.gipsychampionsapp.modelo.Equipo;
 
 /**
  * Clase de ejecución del programa de equipos
+ * 
  * @author Sandoval y Lobato
  * @version 1.0
  */
@@ -15,24 +16,25 @@ public class App {
 
     /**
      * Función principal para la ejecución del programa
+     * 
      * @param args posibles argumentos introducidos
      */
     public static void main(String[] args) {
 
-        //atributo escáner
+        // atributo escáner
         Scanner sc = new Scanner(System.in);
-        //instancia del objeto EquipoDAO
+        // instancia del objeto EquipoDAO
         EquipoDAO equipoDAO = new EquipoDAO();
-        //List en el que almacenaremos la colección de equipos
+        // List en el que almacenaremos la colección de equipos
         List<Equipo> listaEquipo = equipoDAO.getAllEquipos();
 
-        //atributos que se usarán para responder el menú
+        // atributos que se usarán para responder el menú
         int respuesta = 0;
         int respuesta2 = 0;
 
-        //Bucle que se repetirá hasta que la opción introducida sea la de salir
+        // Bucle que se repetirá hasta que la opción introducida sea la de salir
         do {
-            //impresión del menú
+            // impresión del menú
             System.out.println("\nLIGA MORROCOTUDA");
             System.out.println("----------------");
             System.out.println("1. Equipo Random");
@@ -40,7 +42,8 @@ public class App {
             System.out.println("3. Todos los equipos");
             System.out.println("4. Salir");
 
-            //almacena la respuesta introducida por teclado y controla que no sea incorrecta, en caso de que sí lo fuese, mostraría un error.
+            // almacena la respuesta introducida por teclado y controla que no sea
+            // incorrecta, en caso de que sí lo fuese, mostraría un error.
             try {
                 respuesta = sc.nextInt();
             } catch (InputMismatchException e) {
@@ -49,12 +52,15 @@ public class App {
                 respuesta = 0;
             }
 
+            // Switch que comprobará la entrada introducida anteriormente
             switch (respuesta) {
 
+                // Llama al método "getEquipoAleatorio"
                 case 1:
                     System.out.println(equipoDAO.getEquipoAleatorio());
                     break;
-
+                // Imprime un pequeño menú con el id de los equipos y permite introducir un
+                // nombre para ver los datos de este
                 case 2:
                     for (int i = 0; i < listaEquipo.size(); i++) {
                         System.out.println((i + 1) + ". " + listaEquipo.get(i).getNombre());
@@ -74,24 +80,24 @@ public class App {
                         System.out.println("Ese número no está en el índice, compañerete");
                     }
                     break;
-
+                // Muestra todos los equipos con un for each.
                 case 3:
                     for (Equipo e : listaEquipo) {
                         System.out.println(e);
                     }
                     break;
-
+                // Imprime un mensaje para salir del programa
                 case 4:
                     System.out.println("Saliendo...");
                     break;
-
+                // Default que se imprimirá en caso de que el valor introducido sea incorrecto.
                 default:
                     System.out.println("Ese número no es válido compañerete");
                     break;
             }
 
         } while (respuesta != 4);
-
+        // cerramos el scanner
         sc.close();
     }
 }
--- src/main/java/org/palomafp/gipsychampionsapp/EquipoDAO.java
+++ src/main/java/org/palomafp/gipsychampionsapp/EquipoDAO.java
@@ -8,12 +8,24 @@ import org.palomafp.gipsychampionsapp.modelo.Equipo;
 import org.palomafp.gipsychampionsapp.modelo.Estadio;
 import org.palomafp.gipsychampionsapp.modelo.Jugador;
 
+/**
+ * Creación de la clase del objeto de acceso a datos
+ * 
+ * @author Sandoval y Lobato
+ */
 public class EquipoDAO {
-
+    // Arraylist privado que almacenará los equipos.
     private ArrayList<Equipo> listaEquipo = new ArrayList<Equipo>();
 
+    /**
+     * Constructor por defecto sin parámetros para la instancia creada de la clase.
+     */
     public EquipoDAO() {
         // Creación Real Vardrid
+        // creamos el equipo instanciando todas las clases necesarias para este y para
+        // rellenarlo.
+        // creamos un arraylist para almacenar a los jugadores del equipo.
+        // añadimos el equipo al arraylist de la clase
         listaEquipo.add(new Equipo(0, "Real Vardrid", "Madrid"));
         Entrenador entrenador1 = new Entrenador(0, "Chavi", "San Ildefonso", "papoi");
         Estadio estadio1 = new Estadio(0, "Anchoneu", "Chamartín");
@@ -25,6 +37,10 @@ public class EquipoDAO {
         listaEquipo.get(0).setJugadores(listaJugadores1);
 
         // Creación Varcelona
+        // creamos el equipo instanciando todas las clases necesarias para este y para
+        // rellenarlo.
+        // creamos un arraylist para almacenar a los jugadores del equipo.
+        // añadimos el equipo al arraylist de la clase
         listaEquipo.add(new Equipo(1, "Varcelona", "Barcelona"));
         Entrenador entrenador2 = new Entrenador(1, "Jansi", "Musty Flick", "Alemán");
         Estadio estadio2 = new Estadio(1, "Campo nuevo", "Rocafonda 304");
@@ -36,6 +52,10 @@ public class EquipoDAO {
         listaEquipo.get(1).setJugadores(listaJugadores2);
 
         // Creación Patético de Madrid
+        // creamos el equipo instanciando todas las clases necesarias para este y para
+        // rellenarlo.
+        // creamos un arraylist para almacenar a los jugadores del equipo.
+        // añadimos el equipo al arraylist de la clase
         listaEquipo.add(new Equipo(2, "Patético de Madrid", "Madrid"));
         Entrenador entrenador3 = new Entrenador(2, "Chulo", "Sin peones", "Mate");
         Estadio estadio3 = new Estadio(2, "Estadio Napolitano", "Canillejas Mierda");
@@ -47,6 +67,10 @@ public class EquipoDAO {
         listaEquipo.get(2).setJugadores(listaJugadores3);
 
         // Creación Los Morrocotudos
+        // creamos el equipo instanciando todas las clases necesarias para este y para
+        // rellenarlo.
+        // creamos un arraylist para almacenar a los jugadores del equipo.
+        // añadimos el equipo al arraylist de la clase
         listaEquipo.add(new Equipo(3, "Los Morrocotudos", "Distrito San Blas-Canillejas-Ciudad Lineal"));
         Entrenador entrenador4 = new Entrenador(3, "MariCarmen", "No me acuerdo", "72839163A");
         Estadio estadio4 = new Estadio(3, "Patio La Paloma", "IES Virgen de la Paloma");
@@ -60,26 +84,62 @@ public class EquipoDAO {
         listaEquipo.get(3).setJugadores(listaJugadores4);
     }
 
+    /**
+     * Método que devolverá el equipo ubicado en el índice especificado del
+     * arraylist
+     * 
+     * @param indice Posición del arraylist sobre el que queremos el equipo
+     * @return Objeto de la clase Equipo posicionado en el indice introducido
+     */
     public Equipo getEquipoPorIndice(int indice) {
         return listaEquipo.get(indice);
     }
 
+    /**
+     * Método para obtener la lista de los equipos del objeto
+     * 
+     * @return Arraylist con los objetos creados anteriormente con el constructor
+     */
     public ArrayList<Equipo> getAllEquipos() {
         return listaEquipo;
     }
 
+    /**
+     * Método para obtener un objeto de la clase Equipo aleatorio
+     * 
+     * @return Objeto de la clase Equipo obtenido aleatoriamente desde el arraylist
+     *         de la clase
+     */
     public Equipo getEquipoAleatorio() {
         return listaEquipo.get((int) Math.random() * (listaEquipo.size() - 1) + 0);
     }
 
+    /**
+     * Método para añadir un nuevo equipo al objeto. Comprueba que el id del equipo
+     * esté disponible antes de añadirlo. En caso de que este no lo estuviese, no lo
+     * añadirá.
+     * 
+     * @param idEquipo     Id del equipo a añadir
+     * @param nombreEquipo Nombre del equipo a añadir
+     * @param ciudadEquipo Ciudad del equipo a añadir
+     * @return boolean que indicará true en caso de haber podido añadir el equipo o
+     *         false en caso de que no.
+     */
     public boolean addEquipo(int idEquipo, String nombreEquipo, String ciudadEquipo) {
+        // Boolean para indicar si el id está disponible
         boolean estaDisponible = true;
+        // bucle que recorre el array de los equipos buscando un equipo con el mismo id
+        // que el que se quiere añadir
         for (int i = 0; i < listaEquipo.size(); i++) {
             if (listaEquipo.get(i).getIdEquipo() == idEquipo) {
+                // Si hay un equipo con el mismo id, establece el estado de la variable boolean
+                // como false.
                 estaDisponible = false;
             }
         }
+        // condicional que comprueba si el id está disponible.
         if (estaDisponible == true) {
+            // añade el equipo al array en caso de estar disponible el id
             listaEquipo.add(new Equipo(idEquipo, nombreEquipo, ciudadEquipo));
         }
         return estaDisponible;
